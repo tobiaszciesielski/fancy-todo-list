bbe73f4a91b07dc0dee5a3690fb4aad19c29b976
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2021-06-18 10:03:45 +0200

    Added controls to each task

--- src/app/todo/components/button/button.component.ts
+++ src/app/todo/components/button/button.component.ts
@@ -1,20 +1,16 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent {
   @Input() text!: string;
   @Input() color!: string;
   @Output() btnClick = new EventEmitter();
 
   constructor() {}
-
-  ngOnInit(): void {
-
-  }
 
   onClick() {
     this.btnClick.emit();
