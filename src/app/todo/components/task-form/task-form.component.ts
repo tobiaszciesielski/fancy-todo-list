0f4f5255dcdceb47bfa45e1bdb27c76789044733
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2021-06-23 13:59:12 +0200

    Initialized task-form component and imported FormsModule

---
+++ src/app/todo/components/task-form/task-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Task } from '../../models/Task';
+
+@Component({
+  selector: 'app-task-form',
+  templateUrl: './task-form.component.html',
+  styleUrls: ['./task-form.component.scss'],
+})
+export class TaskFormComponent implements OnInit {
+  @Input() detail!: Task;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { FormsModule } from '@angular/forms';
 
 // Services
 import { TodoService } from './todo.service';
@@ -9,6 +10,7 @@ import { InMemoryDataService } from './services/inMemoryData/in-memory-data.serv
 
 // Containers
 import { TodoComponent } from './todo.component';
+import { TaskViewerComponent } from './containers/task-viewer/task-viewer.component';
 
 // Components
 import { ButtonComponent } from './components/button/button.component';
@@ -16,22 +18,26 @@ import { HeaderComponent } from './components/header/header.component';
 import { SummaryComponent } from './components/summary/summary.component';
 import { TasksComponent } from './containers/tasks/tasks.component';
 import { TaskComponent } from './components/task/task.component';
-import { TaskViewerComponent } from './containers/task-viewer/task-viewer.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
 
 @NgModule({
   declarations: [
+    // Conteiners
+    TasksComponent,
+    TaskViewerComponent,
+
+    // Compon
     ButtonComponent,
     HeaderComponent,
     SummaryComponent,
-    TasksComponent,
-    TaskViewerComponent,
     TodoComponent,
     TaskComponent,
+    TaskFormComponent,
   ],
   imports: [
     CommonModule,
+    FormsModule,
     HttpClientModule,
-
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
       dataEncapsulation: false,
       delay: 1000,
